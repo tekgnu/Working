1c86b1e9e19a21aba25525c33a0cd5fd85a39cad
Matthew W. Perry <matthew@digitalanomaly.com>
2021-05-11 17:18:42 -0400

    tst

--- azpyenv/azauth.py
+++ azpyenv/azauth.py
@@ -4,11 +4,25 @@
 import configparser
 
 parser = configparser.ConfigParser()
-parser.read('./accounts.ini')
-print(parser.sections())
-if parser.has_section('Account') == False:
-    print("AZ Auth was unable to access the accounts.ini file required for authenticating.")
-    print("See the example Accounts.ini file for more information")
-else:
-    parser.sections()
+
+parser['Account'] = {}
+parser['Account']['TenantId'] = '72f988bf-86f1-41af-91ab-2d7cd011db47'
+parser['Account']['PyServicePrinciple'] ='mwppy-sp-rbac-001'
+parser['Account']['PyPwd'] = 't9Yfi0Bx5_D3wqEwbYR-9sfenidcvr~N2n'
+
+parser['Resources'] = {}
+parser['Resources']['ResourceGroup'] = 'rg-deve1py001'
+parser['Resources']['Location'] ='eastus'
+
+with open('accounts.cfg', 'w') as configfile:
+    parser.write(configfile)
+
+
+#parser.read_file('accounts.cfg')
+#print(parser.sections())
+#if parser.has_section('Account') == False:
+#    print("AZ Auth was unable to access the accounts.cfg file required for authenticating.")
+#    print("See the example Accounts.cfg file for more information")
+#else:
+#    parser.sections()
 #for sect in parser.section():
