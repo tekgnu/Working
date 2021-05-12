b60450e09e7d91fd05fc782e02b934ba1bd1ea3b
Matthew W. Perry <matthew@digitalanomaly.com>
2021-05-12 11:39:25 -0400

    cmt

---
+++ azpyenv/Utils/AccountConfig.py
@@ -0,0 +1,53 @@
+﻿# This class is to create the account configuration.  
+#   By creating it as a separate class it allows it to be run separately, as well as 
+#   Used to create a custom configuration.  
+#   NOTE: Use .gitignore to hide account credentials
+import configparser
+
+class AzAccount:
+
+    # Init with only the keys that the configuration needs to align to
+    def __init__(self):
+        self.accountDef = { 'tenantid': "" , 'pysp': "" , 'pypwd': ""}
+
+    # Creates the Account configuration file with the defined key, value pairs
+    def createAccount(self, file="Accounts.cfg", section="Account", configDict={}):
+        writer = configparser.ConfigParser()
+        writer[section] = {}
+        for key in configDict:
+            writer[section][key] = configDict[key]
+        with open(file, 'w') as configfile:
+            writer.write(configfile)
+    
+    # Outputs the Account file to validate
+    def getAccount(self, file="Accounts.cfg", section="Account"):
+        reader = configparser.ConfigParser()
+        reader.read(file)
+
+        if reader.has_section('Account') == False or reader[section]['tenantid'] == "":
+            print("The Account Configuration is either missing or was not created correctly.")
+        else:    
+            self.accountDef['tenantid'] = reader[section]['tenantid']
+            self.accountDef['pysp'] = reader[section]['pysp']
+            self.accountDef['pypwd'] = reader[section]['pypwd']
+        return self.accountDef
+
+if __name__ == '__main__':
+    file =  input("Name of the config file [def. Accounts.cfg]: ")
+    if file == "":
+        file = "Accounts.cfg"
+    tenid = input("Enter the Azure TenantID:                    ")
+    pysp =  input("Enter the Python Service Principal Name:     ")
+    pypwd = input("Input the Python Service Principal Pwd:      ")
+    if tenid == "" or pysp == "" or pypwd == "":
+        print("Error data was not provided for the Account config file.")
+    else:
+        myazacct = AzAccount()
+        myazacct.createAccount(
+            file=file, section="Account", configDict={ 'tenantid': tenid , 'pysp': pysp , 'pypwd': pypwd})
+        validate = myazacct.getAccount(file=file, section="Account")
+        print("Validating the data input:")
+        print(validate)
+
+
+
--- azpyenv/azauth.py
+++ azpyenv/azauth.py
@@ -1,28 +1,17 @@
 ﻿# This is just a python wrapper for Authenticating via an Azure Service Principle
 #   It is reliant on an accounts.ini - see example for more details
-
+import Utils.AccountConfig as azcfg
 import configparser
+import os
 
-parser = configparser.ConfigParser()
+# This changes the directory to the python virtual env. from the Repo's directory 
+os.chdir('./azpyenv')
+print(os.getcwd())
 
-parser['Account'] = {}
-parser['Account']['TenantId'] = '72f988bf-86f1-41af-91ab-2d7cd011db47'
-parser['Account']['PyServicePrinciple'] ='mwppy-sp-rbac-001'
-parser['Account']['PyPwd'] = 't9Yfi0Bx5_D3wqEwbYR-9sfenidcvr~N2n'
+authCfg = azcfg.AzAccount()
+auth = authCfg.getAccount('Accounts.cfg')
+print(auth)
 
-parser['Resources'] = {}
-parser['Resources']['ResourceGroup'] = 'rg-deve1py001'
-parser['Resources']['Location'] ='eastus'
+print("End")
 
-with open('Accounts.cfg', 'w') as configfile:
-    parser.write(configfile)
-
-
-#parser.read_file('accounts.cfg')
-#print(parser.sections())
-#if parser.has_section('Account') == False:
-#    print("AZ Auth was unable to access the accounts.cfg file required for authenticating.")
-#    print("See the example Accounts.cfg file for more information")
-#else:
-#    parser.sections()
-#for sect in parser.section():
+    
